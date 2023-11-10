e489ea44120b178db8b152ed23ff2ee52f31a072
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2023-11-10 16:26:39 +0100

    Move client/server logic to dedicated classes, Add REPL commands input

---
+++ src/main/java/ch/heigvd/Hangman.java
@@ -0,0 +1,33 @@
+package ch.heigvd;
+
+import picocli.CommandLine;
+
+import java.util.Scanner;
+
+public abstract class Hangman implements Runnable {
+    @CommandLine.Option(names = {"-p", "--port"}, description = "defines a specific port to use for communication")
+    protected int portNum = 12345;
+
+    public void startRepl() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the REPL. Enter 'exit' to end.");
+
+        while (true) {
+            System.out.print("Enter a command: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting REPL. Goodbye!");
+                break;
+            }
+
+            // Process the user input (you can replace this with your command processing logic)
+            processReplCommand(userInput);
+        }
+
+        scanner.close();
+    }
+
+    protected abstract void processReplCommand(String command);
+}
--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -10,39 +10,7 @@ public class Main {
 }
 
 @CommandLine.Command(name = "Hangman",
-        subcommands = {ClientSubcommand.class, ServerSubcommand.class, CommandLine.HelpCommand.class},
+        subcommands = {Client.class, Server.class, CommandLine.HelpCommand.class},
         description = "Run a game of Hangman")
 class HangmanGame {
-
-}
-
-abstract class HangmanCommand implements Runnable {
-    @CommandLine.Option(names = {"-p", "--port"}, description = "defines a specific port to use for communication")
-    protected int portNum = 12345;
-}
-
-@CommandLine.Command(name = "server",
-        description = "starts a server for a game of Hangman")
-class ServerSubcommand extends HangmanCommand {
-    @CommandLine.Option(names = {"-t", "--thread"}, description = "defines a number of thread to use for the server")
-    private int threadNbr = 5;
-
-    @Override
-    public void run() {
-        System.out.println("server with " + threadNbr + " threads on " + portNum);
-    }
-}
-
-
-@CommandLine.Command(name = "client",
-        description = "Starts a client for a game of Hangman")
-class ClientSubcommand extends HangmanCommand {
-    @Override
-    public void run() {
-        System.out.println("client on " + portNum);
-        int length = 4;
-        String word = Utils.findWord(Utils.Language.EN, length);
-        System.out.println("Random word with " + length + " letters: " + word);
-
-    }
 }
