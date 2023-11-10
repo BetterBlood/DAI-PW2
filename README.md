338eaf70ad96f53ec7045f712182db644eb186bf
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2023-11-10 14:03:08 +0100

    Implement basic PicoCLI server/client subcommands

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# WIP
+# Hangman
+
+## Instructions
+
+automatic client
+```bash
+java -jar target/dai-pw2-1.0-SNAPSHOT.jar client
+```
+
+client with parameters
+```bash
+java -jar target/dai-pw2-1.0-SNAPSHOT.jar client -p 666
+```
+
+automatic server
+```bash
+java -jar target/dai-pw2-1.0-SNAPSHOT.jar server
+```
+
+server with parameters
+```bash
+java -jar target/dai-pw2-1.0-SNAPSHOT.jar server -p 666 -t 6
+```
--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,7 +1,44 @@
 package ch.heigvd;
 
+import picocli.CommandLine;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new HangmanGame()).execute(args);
+        System.exit(exitCode);
+    }
+}
+
+@CommandLine.Command(name = "Hangman",
+        subcommands = {ClientSubcommand.class, ServerSubcommand.class},
+        description = "Run a game of Hangman")
+class HangmanGame {
+
+}
+
+abstract class HangmanCommand implements Runnable {
+    @CommandLine.Option(names={"-p", "--port"}, description = "defines a specific port to use")
+    protected int portNumber = 12345;
+}
+
+@CommandLine.Command(name = "server",
+        description = "starts a server for a game of Hangman")
+class ServerSubcommand extends HangmanCommand {
+    @CommandLine.Option(names={"-t", "--thread"}, description = "defines a number of thread to use for the server")
+    private int threadNumber = 5;
+
+    @Override
+    public void run() {
+        System.out.println("server with " + threadNumber + " threads on " + portNumber);
+    }
+}
+
+
+@CommandLine.Command(name = "client",
+        description = "Starts a client for a game of Hangman")
+class ClientSubcommand extends HangmanCommand {
+    @Override
+    public void run() {
+        System.out.println("client on " + portNumber);
     }
 }
